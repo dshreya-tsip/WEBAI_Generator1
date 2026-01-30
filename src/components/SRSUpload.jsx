f8b70b11a5f4d8b9187199a6b0dd0ec180ad0187
dshreya-tsip <Shreya.Dhavule@toshiba-tsip.com>
2026-01-30 11:07:25 +0530

    Update SRSUpload.jsx

--- src/components/SRSUpload.jsx
+++ src/components/SRSUpload.jsx
@@ -1,1 +1,15 @@
 
+import React from "react";
+
+export default function SRSUpload({ onUpload }) {
+  const handleFileChange = (e) => {
+    onUpload(e.target.files[0]);
+  };
+
+  return (
+    <div className="card">
+      <h3>Upload SRS Document</h3>
+      <input type="file" accept=".pdf,.docx,.txt" onChange={handleFileChange} />
+    </div>
+  );
+}
