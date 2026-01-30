0ca0afe191237532184a3939283d873864658bcc
dshreya-tsip <Shreya.Dhavule@toshiba-tsip.com>
2026-01-30 11:08:17 +0530

    Update TemplateUpload.jsx

--- src/components/TemplateUpload.jsx
+++ src/components/TemplateUpload.jsx
@@ -1,1 +1,9 @@
+export default function TemplateUpload({ onUpload }) {
+  return (
+    <div className="card">
+      <h3>Upload Test Case Template</h3>
+      <input type="file" accept=".xlsx,.docx" onChange={(e) => onUpload(e.target.files[0])} />
+    </div>
+  );
+}
 
