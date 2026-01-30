c0968d9953ef47d41355dced00d92b6f90272c13
dshreya-tsip <Shreya.Dhavule@toshiba-tsip.com>
2026-01-30 11:07:00 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:8000/api"; // change later
+
+export const uploadSRS = (formData) =>
+  axios.post(`${API_BASE}/upload-srs`, formData);
+
+export const generateTestCases = (payload) =>
+  axios.post(`${API_BASE}/generate`, payload);
+
+export const downloadTestCases = (id) =>
+  axios.get(`${API_BASE}/download/${id}`, { responseType: "blob" });
 
