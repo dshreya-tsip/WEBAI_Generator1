69a46fb6255f45e773bcfd843e946081a8745795
dshreya-tsip <Shreya.Dhavule@toshiba-tsip.com>
2026-01-30 11:09:05 +0530

    Update DownloadPanel.jsx

--- src/components/DownloadPanel.jsx
+++ src/components/DownloadPanel.jsx
@@ -1,1 +1,8 @@
+export default function DownloadPanel({ onDownload }) {
+  return (
+    <div className="card">
+      <button onClick={onDownload}>Download Test Cases</button>
+    </div>
+  );
+}
 
